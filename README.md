e88a108476381853c6729a4231eaa726f44621c5
nokhodian <nokhodian@gmail.com>
2018-12-04 21:01:52 +0100

    first and hopefully last commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # proxyAPI
+
+You can reroute all your APIs to another server for logging, bypassing filters or any other reasons you might have
+
+simply set the server URL here and npm install and node index.js and you have it up and running
---
+++ index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+
+
+var PORT = process.env.PORT || 3000;
+var app = express();
+app.use(bodyParser.json({}));
+app.listen(PORT, function () {
+  console.log('node proxy listening on port %s.', PORT);
+});
+
+
+//set your Server URL here
+const BASE_URL = 'http://localhost:2018';
+
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+  const options = {
+    url: BASE_URL + req.path,
+    method: req.method,
+    body: req.body,
+    headers: {
+      'content-type': req.headers['content-type'],
+      authorization: req.headers['authorization']
+    },
+    json: true
+  };
+
+
+  request(options, function (error, response, body) {
+    if (response.statusCode == 200) {
+      console.log("The following request and response has been served successfuly")
+      console.log(options)
+      console.log(body)
+      res.send(body);
+    } else {
+      console.log('error: ' + response.statusCode);
+      console.log(options)
+      console.log(body)
+      res.send(body).status(response.statusCode);
+    }
+  })
+});
